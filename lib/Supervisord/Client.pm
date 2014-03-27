cf8c283b6290b34d4055471a9a04817ded76ecb5
skaufman <sam@socialflow.com>
2014-03-27 02:33:52 +0000

    docs

--- lib/Supervisord/Client.pm
+++ lib/Supervisord/Client.pm
@@ -55,7 +55,11 @@ sub AUTOLOAD {
     my $remote_method = $AUTOLOAD;
     $remote_method =~ s/.*:://;
     my ( $self, @args ) = @_;
-    my $ret = $self->rpc->send_request( "supervisor.$remote_method", @args );
+    $self->send_rpc_request("supervisor.$remote_method", @args );
+}
+sub send_rpc_request {
+    my( $self, @params ) = @_;
+    my $ret = $self->rpc->send_request( @params );
     return $ret->value if $ret->$_can("value");
 }
 
@@ -64,3 +68,61 @@ sub AUTOLOAD {
 =head1 NAME
 
 Supervisord::Client - a perl client for Supervisord's XMLRPC.
+
+=head1 SYNOPSIS
+
+    my $client = Supervisord::Client->new( serverurl => "unix:///tmp/socky.sock" );
+    #or
+    my $client = Supervisord::Client->new( path_to_supervisor_config => "/etc/supervisor/supervisor.conf" );
+    warn $_->{description} for(@{ client->getAllProcessInfo });
+    #or
+    warn $_->{description} for(@{ client->send_rpc_request("supervisor.getAllProcessInfo") });
+
+=head1 DESCRIPTION
+
+This module is for people who are using supervisord (  L<http://supervisord.org/> ) to manage their daemons,
+and are using the unix socket form of the RPC ( as opposed to http server ).
+This module will work with either, but really you're not getting much vs RPC::XML::Client for the http version;
+the http over Unix socket part is where this module comes in handy.
+See L<http://supervisord.org/api.html> for the API docs.
+
+=head1 METHODS
+
+=head2 new
+
+Constructor, provided by Moo.
+
+=head2 rpc
+
+Access to the RPC::XML::Client object.
+
+=head2 send_rpc_request( remote_method, @params )
+
+=head2 AUTOLOAD
+
+This module uses AUTOLOAD to proxy calls to send_rpc_request. See synopsis for examples.
+
+=head1 CONSTRUCTOR PARAMETERS
+
+=over
+
+=item path_to_supervisor_config
+
+optional - ex: /tmp/super.sock
+
+=item serverurl
+
+optional - in supervisor format, ex: unix:///tmp.super.sock
+
+One of the two is required.
+
+=back
+
+=head1 LICENSE
+
+This library is free software and may be distributed under the same terms as perl itself.
+
+=head1 AUTHOR
+
+Samuel Kaufman L<skaufman@cpan.org>
+

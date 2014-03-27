df1c7fa9537bc03d6cbaeb2168e0c34dd4fbf0d5
skaufman <sam@socialflow.com>
2014-03-27 01:19:41 +0000

    tidied

--- lib/Supervisord/Client.pm
+++ lib/Supervisord/Client.pm
@@ -9,19 +9,20 @@ use Carp;
 use Safe::Isa;
 use Config::INI::Reader;
 
-LWP::Protocol::implementor( supervisorsocketunix => 'LWP::Protocol::http::SocketUnixAlt' );
+LWP::Protocol::implementor(
+    supervisorsocketunix => 'LWP::Protocol::http::SocketUnixAlt' );
 
 has path_to_supervisor_config => (
-    is => 'ro',
+    is       => 'ro',
     required => 0,
 );
 
 has serverurl => (
-    is => 'ro',
+    is       => 'ro',
     required => 0,
 );
 
-has rpc => ( is => 'lazy' );
+has rpc        => ( is => 'lazy' );
 has _serverurl => ( is => 'lazy' );
 
 sub _build__serverurl {
@@ -33,9 +34,10 @@ sub _build__serverurl {
       || croak "couldnt find serverurl in supervisorctl section of "
       . $self->path_to_supervisor_config;
 }
+
 sub _build_rpc {
     my $self = shift;
-    my $url = $self->_serverurl;
+    my $url  = $self->_serverurl;
     $url =~ s|unix://|supervisorsocketunix:|g;
     $url .= "//RPC2";
     my $cli = RPC::XML::Client->new($url);
@@ -43,10 +45,13 @@ sub _build_rpc {
 
 sub BUILD {
     my $self = shift;
-    $self->path_to_supervisor_config || $self->serverurl || croak "path_to_supervisor_config or serverurl required.";
+    $self->path_to_supervisor_config
+      || $self->serverurl
+      || croak "path_to_supervisor_config or serverurl required.";
 }
 
 our $AUTOLOAD;
+
 sub AUTOLOAD {
     my $remote_method = $AUTOLOAD;
     $remote_method =~ s/.*:://;
@@ -55,16 +60,8 @@ sub AUTOLOAD {
     return $ret->value if $ret->$_can("value");
 }
 
-
-
-#my $path = "file:///home/skaufman/dev/Supervisord-Client/example/supervisor.sock";
-##my $resp = $cli->send_request('supervisor.getVersion');
-#my $resp = $cli->send_request('supervisor.getAllProcessInfo');
-#warn Dumper $resp->value;
-##my $req =
-##"POST /RPC2 HTTP/1.1\r\nHost: localhost\r\nAccept-Encoding: identity\r\nContent-Length: 115\r\nContent-Type: text/xml\r\nAccept: text/xml\r\nUser-Agent: xmlrpclib.py/1.0.1 (by www.pythonware.com)\r\n\r\n<?xml version='1.0'?>\n<methodCall>\n<methodName>supervisor.getVersion</methodName>\n<params>\n</params>\n</methodCall>\n";
-#
 1;
+
 =head1 NAME
 
 Supervisord::Client - a perl client for Supervisord's XMLRPC.

856af2d231b3e04fde0ac009c3b816eaaaccb522
skaufman <sam@socialflow.com>
2014-03-27 01:18:04 +0000

    supervisor ini reading

--- lib/Supervisord/Client.pm
+++ lib/Supervisord/Client.pm
@@ -7,6 +7,7 @@ use RPC::XML::Client;
 use Moo::Lax;
 use Carp;
 use Safe::Isa;
+use Config::INI::Reader;
 
 LWP::Protocol::implementor( supervisorsocketunix => 'LWP::Protocol::http::SocketUnixAlt' );
 
@@ -21,13 +22,22 @@ has serverurl => (
 );
 
 has rpc => ( is => 'lazy' );
+has _serverurl => ( is => 'lazy' );
 
+sub _build__serverurl {
+    my $self = shift;
+    return $self->serverurl if $self->serverurl;
+    my $hash =
+      Config::INI::Reader->read_file( $self->path_to_supervisor_config );
+    return $hash->{supervisorctl}{serverurl}
+      || croak "couldnt find serverurl in supervisorctl section of "
+      . $self->path_to_supervisor_config;
+}
 sub _build_rpc {
     my $self = shift;
-    my $url = $self->serverurl;
+    my $url = $self->_serverurl;
     $url =~ s|unix://|supervisorsocketunix:|g;
     $url .= "//RPC2";
-    warn $url;
     my $cli = RPC::XML::Client->new($url);
 }
 
--- script/ctl.pl
+++ script/ctl.pl
@@ -2,20 +2,6 @@ use strict;
 use warnings FATAL => 'all';
 use Supervisord::Client;
 use Data::Dumper::Concise;
-my $client = Supervisord::Client->new( serverurl => $ARGV[0] );
-#warn $client->AUTOLOAD;w
+#my $client = Supervisord::Client->new( serverurl => $ARGV[0] );
+my $client = Supervisord::Client->new( path_to_supervisor_config => $ARGV[0] );
 warn Dumper $client->getAllProcessInfo;
-#warn Dumper $client::getAsdfs;
-#use LWP::Protocol::http::SocketUnix;
-#use LWP::UserAgent;
-#use Data::Dumper::Concise;
-#use RPC::XML::Client;
-#LWP::Protocol::implementor( file => 'LWP::Protocol::http::SocketUnix' );
-##my $ua = LWP::UserAgent->new;
-#my $path = "file:///home/skaufman/dev/Supervisord-Client/example/supervisor.sock";
-#my $cli = RPC::XML::Client->new($path);
-##my $resp = $cli->send_request('supervisor.getVersion');
-#my $resp = $cli->send_request('supervisor.getAllProcessInfo');
-#warn Dumper $resp->value;
-##my $req =
-##"POST /RPC2 HTTP/1.1\r\nHost: localhost\r\nAccept-Encoding: identity\r\nContent-Length: 115\r\nContent-Type: text/xml\r\nAccept: text/xml\r\nUser-Agent: xmlrpclib.py/1.0.1 (by www.pythonware.com)\r\n\r\n<?xml version='1.0'?>\n<methodCall>\n<methodName>supervisor.getVersion</methodName>\n<params>\n</params>\n</methodCall>\n";

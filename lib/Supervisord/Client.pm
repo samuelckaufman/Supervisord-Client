7ceec741010ae7879c94d72ff999aa960d05fcaf
skaufman <sam@socialflow.com>
2014-11-20 20:45:11 +0000

    changelog

--- lib/Supervisord/Client.pm
+++ lib/Supervisord/Client.pm
@@ -52,8 +52,7 @@ sub _build_rpc {
     my $uri = URI->new( $url );
     if( lc($uri->scheme) eq 'unix' ) {
         my $socket_uri = URI->new("supervisorsocketunix:");
-        my @old_segments = grep { $_ } $uri->path_segments;
-        $socket_uri->path_segments( @old_segments, "", "RPC2" );
+        $socket_uri->path_segments( $uri->path_segments, "", "RPC2" );
         $uri = $socket_uri;
     } else {
         $uri->path_segments( $uri->path_segments, "RPC2" );
